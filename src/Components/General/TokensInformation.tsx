0e5193c7d52b74374a64884e3576747e7358d51c
Adeel Ahmad <adeel@0xa.co.uk>
2024-08-11 14:22:17 +0100

    add ability for user to search for brc20 token

--- src/Components/General/TokensInformation.tsx
+++ src/Components/General/TokensInformation.tsx
@@ -1,14 +1,14 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { Tabs } from '../Tabs/Tabs';
-import { tokens } from '@/constants';
-
-const tickers = tokens;
+import { tokens as initialTokens } from '@/constants';
 
 export default function TokensInformation() {
+  const [tickers, setTickers] = useState<string[]>(initialTokens);
   const [tokenData, setTokenData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [newTicker, setNewTicker] = useState<string>('');
 
   const fetchTokenData = async () => {
     try {
@@ -31,11 +31,34 @@ export default function TokensInformation() {
     fetchTokenData();
   }, [tickers]);
 
+  const handleAddTicker = () => {
+    if (newTicker && !tickers.includes(newTicker)) {
+      setTickers((prev) => [...prev, newTicker.toLowerCase()]);
+      setNewTicker('');
+    }
+  };
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
   return (
     <div className="flex flex-col items-center mt-5 gap-5">
+      <div className="flex items-center gap-2 w-full justify-between">
+        <input
+          type="text"
+          value={newTicker}
+          onChange={(e) => setNewTicker(e.target.value)}
+          placeholder="Enter token ticker"
+          className="border border-gray-300 px-3 py-1 rounded-xl text-sm"
+        />
+        <button
+          onClick={handleAddTicker}
+          className="px-4 py-2 rounded-xl text-xs bg-slate-300"
+        >
+          Add Token
+        </button>
+      </div>
+
       <Tabs tabs={tokenData} />
     </div>
   );
